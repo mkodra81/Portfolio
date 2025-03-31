a2042e7bb0d87d51d7d64a5e5ddf4ac01da80f0a
Mateo <kodramateo4@gmail.com>
2025-03-31 21:31:29 +0200

    Navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 
 const Navbar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
